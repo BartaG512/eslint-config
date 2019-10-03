c3a9787ca93f359fea9008a7103479fc6d8ae54b
barta.gergo <barta.gergo@gammadigital.hu>
2019-10-03 14:50:58 +0200

    Add rule for no space before or after semicolon

--- index.js
+++ index.js
@@ -93,6 +93,7 @@ module.exports = {
 		"id-length": ["error", { "min": 1, "max": 40, "exceptions": ["i"] }],
 		"spaced-comment": "error",
 		"space-before-function-paren": ["error", "never"],
+		"semi-spacing": ["error", {"before": false, "after": false}],
 		"no-multi-spaces": ["error"],
 		"brace-style": "error",
 		"no-else-return": "error",
