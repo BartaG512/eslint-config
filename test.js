812521a560cde33eae4901279720c87a83a0d92a
barta.gergo <barta.gergo@gammadigital.hu>
2020-03-04 21:16:22 +0100

    clean up and refactoring

--- test.js
+++ test.js
@@ -2,17 +2,26 @@ class ClassName {
 	constructor() {
 		const variable = 1;
 		const hey = { a: 1, b: 2 };
-		const hello = {
-			...hey,
-			variable,
+		const hello = {	...hey,	variable };
+		const obj0 = { foo: "foo", bar: "bar", baz: "baz" };
+		const obj1 = {
+			foo: "foo",
+			bar: "bar",
+			baz: "baz",
 		};
+		const newObject = {
+			a: 'a.m.', b: 'p.m.', c: 1, d: 4,
+			c: 'daylight saving time',
+		};
+		console.log("hello", hello);
 		const promise = new Promise((resolve, reject) => {
-
+			`hello, ${people.name}!`;
 		});
 		const m = async() => {
 			await promise();
 			const m = "hey";
 		};
+		Symbol('1');
 	}
 }
-getTestConfig;
+console.log("ClassName", ClassName);
