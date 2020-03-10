27d856874a70aabb9b4c5b27242bde3fab1d6192
barta.gergo <barta.gergo@gammadigital.hu>
2020-03-10 15:15:30 +0100

    Add new rules

--- test.js
+++ test.js
@@ -1,9 +1,16 @@
-class ClassName {
-	constructor() {
-		const variable = 1;
-		const hey = { a: 1, b: 2 };
-		const hello = {	...hey,	variable };
 
+class ClassName extends Hello {
+	constructor() {
+		this.he = 1;
+		super();
+		const variable = 1;
+		const hey = { a: 1, c: 2, f: 3, b: 2 };
+		const hello = {
+			...hey,
+			variable,
+		};
+		const hello1 = '121';
+		const hello12 = 'dfsfdgh';
 		// this.logger.info('hello', '11');
 		const logger = () => {
 
@@ -11,7 +18,11 @@ class ClassName {
 		logger.info('hello', 'mize');
 		this.logger.info('Message');
 		logger.info('hello', ['1']);
-		logger.warn('hello', { error: 1 });
+		logger.warn('hello', {
+			error: 1,
+			a: 1,
+			b: 2,
+		});
 		console.warn('hello', ['1']);
 		this.logger.info('Message', 'Hello');
 		// this.logger.info('hello', { what: "isLove" });
@@ -20,14 +31,20 @@ class ClassName {
 }
 // console.log("ClassName", ClassName);
 (async() => {
-	console.log("yes");
+	console.log('yes');
 })();
 const red = 1;
+
 if (red === 1) {
 
 }
 
-if (red === "red") {
+if (red === 'red') {
 
 }
-
+const text = 4;
+const expr = 4;
+let m = 4;
+while (m = text.match(expr)) {
+	// ...
+}
